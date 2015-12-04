60ad6de59c15834effaf29611ae24becf68ac4a5
gdomorski <greg.domorski@gmail.com>
2015-12-04 15:55:10 -0800

    Made circles move randomly

--- client/watchout.js
+++ client/watchout.js
@@ -1,1 +1,53 @@
 // start slingin' some d3 here.
+var width = 800;
+var height = 500;
+
+var canvas = d3.select('body').append('svg').attr('width',width).attr('height', height);
+
+var enemyObjects = [];
+for(var i = 0; i < 10; i++){
+  enemyObjects.push(
+    {
+      name: i,
+      x: Math.floor(Math.random()*width),
+      y: Math.floor(Math.random()*height)
+    }
+  );
+};
+
+var update = function(enemyCoordinates){
+  //JOIN Data to DOM elements
+  var enemies = d3.select('svg').selectAll('.enemy')
+    .data(enemyCoordinates, function(d){
+      return d.name;
+    })
+  console.log("logged");
+  //UPDATE
+  enemies.transition().duration(1500)
+    .attr('cx', function(d){
+      d.x = Math.random()*width;
+      return d.x})
+    .attr('cy', function(d){
+      d.y = Math.random()*height;
+      return d.y})
+
+  
+  //ENTER
+  enemies.enter()
+    .append('circle')
+    .attr('r',10)
+    .attr('cx', function(d){return d.x})
+    .attr('cy', function(d){return d.y})
+    .attr('class','enemy');
+
+  //enter and update
+  //exit;
+
+}
+//initial call to setup enemies
+update(enemyObjects);
+
+
+//enemy movement
+setInterval(function(){ update(enemyObjects)},1000);
+
